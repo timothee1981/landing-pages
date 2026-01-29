1973408fb36183001eed177ec5da021b1fc2408c
Timothee <timotheebusch@hotmail.com>
2026-01-29 21:56:15 +0100

    Add Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel"; // Add this
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  output: 'server',
+   adapter: vercel(),
   build: {
     assets: 'assets'
   },
